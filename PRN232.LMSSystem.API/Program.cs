1531e7dd20db524b8937fcdff33a37e0b8783ca7
DangQuangSE <quangndse180246@fpt.edu.vn>
2026-06-01 17:52:24 +0700

    feat: update Swagger security definition to use HTTP scheme and localized description for JWT token input

--- PRN232.LMSSystem.API/Program.cs
+++ PRN232.LMSSystem.API/Program.cs
@@ -139,11 +139,11 @@ builder.Services.AddSwaggerGen(options =>
     options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
         Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
         BearerFormat = "JWT",
         In = ParameterLocation.Header,
-        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\nEnter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\""
+        Description = "Nhập JWT token vào ô bên dưới (KHÔNG cần gõ 'Bearer ', Swagger tự thêm)."
     });
 
     options.AddSecurityRequirement(new OpenApiSecurityRequirement
